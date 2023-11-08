dcb50b5b3d150dd7cc033a6faa45c77ec9d34521
Salvador Guzman <guzmansalv@gmail.com>
2023-11-08 11:34:14 -0800

    decomposed parse func

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,63 @@ enum Expr {
     Mult(Vec<Expr>),
 }
 
+// This function remains the same
 fn parse(input: &str) -> Result<Expr, String> {
     let mut chars = input.chars().peekable();
     parse_expr(&mut chars)
 }
 
+// Refactor parse_expr to handle the opening and closing of expressions
 fn parse_expr(chars: &mut std::iter::Peekable<Chars>) -> Result<Expr, String> {
     skip_whitespace(chars);
     if chars.peek() == Some(&'(') {
         chars.next(); // consume '('
-        skip_whitespace(chars);
-        let expr = match chars.next() {
-            Some('a') => {
-                chars.next(); // consume 'd'
-                chars.next(); // consume 'd'
-                parse_list(chars).map(Expr::Add)
-            }
-            Some('m') => {
-                chars.next(); // consume 'u'
-                chars.next(); // consume 'l'
-                chars.next(); // consume 't'
-                parse_list(chars).map(Expr::Mult)
-            }
-            _ => return Err("Expected 'add' or 'mult'".to_string()),
-        };
+        let expr = parse_inner_expr(chars)?;
         skip_whitespace(chars);
         if chars.next() != Some(')') {
             return Err("Expected ')'".to_string());
         }
-        expr
+        Ok(expr)
     } else {
         parse_number(chars)
     }
 }
+
+// New function to handle the inner part of an expression
+fn parse_inner_expr(chars: &mut std::iter::Peekable<Chars>) -> Result<Expr, String> {
+    skip_whitespace(chars);
+    match chars.next() {
+        Some('a') => {
+            expect_keyword(chars, "dd")?;
+            parse_list(chars).map(Expr::Add)
+        }
+        Some('m') => {
+            expect_keyword(chars, "ult")?;
+            parse_list(chars).map(Expr::Mult)
+        }
+        _ => Err("Expected 'add' or 'mult'".to_string()),
+    }
+}
+
+// New function to check for specific keywords
+fn expect_keyword(chars: &mut std::iter::Peekable<Chars>, keyword: &str) -> Result<(), String> {
+    for expected_char in keyword.chars() {
+        match chars.next() {
+            Some(ch) if ch == expected_char => continue,
+            _ => return Err(format!("Expected '{}'", keyword)),
+        }
+    }
+    Ok(())
+}
+
+// parse_list and parse_number can remain mostly unchanged
+// ...
+
+// The eval function and its helpers can remain unchanged for now
+// ...
+
+// The skip_whitespace function remains the same
+// ...
 
 fn parse_list(chars: &mut std::iter::Peekable<Chars>) -> Result<Vec<Expr>, String> {
     let mut list = Vec::new();
